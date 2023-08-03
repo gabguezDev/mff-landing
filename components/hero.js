10bb0967a27979e25fcf648500a1ce6b67a72459
Gabriel Dominguez <gabriel.dominguez.dev@gmail.com>
2023-08-02 22:10:44 -0300

    first modifications

--- components/hero.js
+++ components/hero.js
@@ -8,13 +8,13 @@ const Hero = () => {
 			<Container className="flex flex-wrap ">
 				<div className="flex items-center w-full lg:w-1/2">
 					<div className="max-w-2xl mb-8">
-						<h1 className="font-adleryPro text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+						<h1 className="font-adleryPro text-4xl font-bold leading-snug tracking-tight text-purple-500 font-thin lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
 							Lic. María Fernanda Franco
 						</h1>
 						<p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-							Nextly is a free landing page & marketing website template for
-							startups and indie projects. Its built with Next.js & TailwindCSS.
-							And its completely open-source.
+							¡Hola! Soy María Fernanda, Licenciada en psicología M.P. N° 1234.
+							Brindo atenciones asdasd asdasd asdasdasda asdasdas asdasdas adsd
+							asda a sdasdasdasd
 						</p>
 
 						<div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
--- components/navbar.js
+++ components/navbar.js
@@ -21,15 +21,9 @@ const Navbar = () => {
 						<>
 							<div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
 								<Link href="/">
-									<span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-										<span>
-											<Image
-												src="/img/logo.svg"
-												alt="N"
-												width="32"
-												height="32"
-												className="w-8"
-											/>
+									<span className="flex items-center space-x-2 text-2xl font-medium text-purple-500 dark:text-gray-100">
+										<span className="bg-purple-500 text-white pl-2 pr-4 pt-3 font-adleryPro rounded">
+											MF
 										</span>
 										<span>Lic. María Fernanda Franco</span>
 									</span>
@@ -37,7 +31,7 @@ const Navbar = () => {
 
 								<Disclosure.Button
 									aria-label="Toggle Menu"
-									className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
+									className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-purple-500 focus:text-purple-600 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
 								>
 									<svg
 										className="w-6 h-6 fill-current"
@@ -66,14 +60,14 @@ const Navbar = () => {
 											<Link
 												key={index}
 												href="/"
-												className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none"
+												className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-purple-500 focus:text-purple-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none"
 											>
 												{item}
 											</Link>
 										))}
 										<Link
 											href="/"
-											className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5"
+											className="w-full px-6 py-2 mt-3 text-center text-white bg-purple-500 rounded-md lg:ml-5"
 										>
 											Empecemos
 										</Link>
@@ -91,7 +85,7 @@ const Navbar = () => {
 							<li className="mr-3 nav__item" key={index}>
 								<Link
 									href="/"
-									className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+									className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-purple-500 focus:text-purple-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
 								>
 									{menu}
 								</Link>
@@ -103,7 +97,7 @@ const Navbar = () => {
 				<div className="hidden mr-3 space-x-4 lg:flex nav__item">
 					<Link
 						href="/"
-						className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5"
+						className="px-6 py-2 text-white bg-purple-600 rounded-md md:ml-5"
 					>
 						Empecemos
 					</Link>
